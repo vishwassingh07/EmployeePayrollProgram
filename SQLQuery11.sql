40e8e07a1caf42cfd5c47fb4c7e3b7645ae2d838
Vishwas <baghel.vishwas0@gmail.com>
2022-07-20 11:46:55 +0530

    [Vishwas]add: Alotting Two Department To The Same Employee

---
+++ SQLQuery11.sql
@@ -0,0 +1,5 @@
+----------- UC10- Giving Two Department To Same Employee ----------
+Insert Into Employee_Payroll_Table (Name, Basic_Pay, StartDate, Gender, Phone, Department, Address, Deductions, Taxable_Pay, Income_Tax , Net_Pay)
+Values ('Vivek',80000,'2019-01-10','M',8434384343,'HR','Pune',10000,70000,6000,64000);
+
+Select * From Employee_Payroll_Table;
